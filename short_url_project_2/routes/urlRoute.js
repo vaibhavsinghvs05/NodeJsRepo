7bbe12424afd484f8e73e0eb592f1e4d5816b43b
Vaibhav <vaibhavsingh.vs05@gmail.com>
2024-10-06 19:39:38 +0530

    Accept deletion of files

--- short_url_project_2/routes/urlRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { urlPost, urlGet, urlGetClicks } from "../controllers/urlController.js";
-
-const router = express.Router(); 
-router.route('/').post(urlPost);
-router.route("/:shortId").get(urlGet);
-router.route("/analytics/:shortId", urlGetClicks);
-export default router;
